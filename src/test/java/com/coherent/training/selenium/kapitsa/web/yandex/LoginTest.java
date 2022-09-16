d835a781149c6caf19a9b7cd74dccdc3a9df4b14
roma0593 <romakapica1993@gmail.com>
2022-09-16 11:24:29 +0200

    create URL provider class

--- src/test/java/com/coherent/training/selenium/kapitsa/web/yandex/LoginTest.java
+++ src/test/java/com/coherent/training/selenium/kapitsa/web/yandex/LoginTest.java
@@ -2,18 +2,21 @@ package com.coherent.training.selenium.kapitsa.web.yandex;
 
 import com.coherent.training.selenium.kapitsa.web.base.BaseTest;
 import com.coherent.training.selenium.kapitsa.web.pages.MainPage;
+import com.coherent.training.selenium.kapitsa.web.providers.UrlProvider;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+import static com.coherent.training.selenium.kapitsa.web.providers.UrlProvider.*;
+
 public class LoginTest extends BaseTest {
 
     @Parameters({"login", "pass"})
     @Test
     public void login(String login, String pass){
-        driver.get("https://mail.yandex.com/");
+        driver.get(YANDEX_MAIL.getUrl());
         mainPage = new MainPage(driver);
 
         loginPage = mainPage.getLoginPage();
